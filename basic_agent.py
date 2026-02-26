2ec96b6f257561e670b3fbd7f7913e39f15abab9
Patrick van Vilsteren <patrick@patrickvanvilsteren.eu>
2026-02-26 21:22:11 +0100

    Basic working agent with Github models

--- basic_agent.py
+++ basic_agent.py
@@ -6,3 +6,14 @@ from agent_framework.openai import OpenAIChatClient
 from dotenv import load_dotenv
 
 load_dotenv()
+
+async def main():
+    client= OpenAIChatClient(env_file_path=".env")
+    agent = Agent(
+        client=client,
+        instructions="You are a helpful assistant which has knowledge about military assets like tanks, planes, ships, etc. You can answer questions about these assets and provide information on their capabilities, specifications, and history."
+    )
+    result = await agent.run("What are the capabilities of the M1 Abrams tank?")
+    print(result)
+
+asyncio.run(main())
