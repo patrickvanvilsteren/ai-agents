d13c16b3bc46522c89955c640a493315a6004d8a
Patrick van Vilsteren <patrick@patrickvanvilsteren.eu>
2026-02-26 21:10:12 +0100

    Installed modules

--- basic_agent.py
+++ basic_agent.py
@@ -3,6 +3,6 @@ import os
 
 from agent_framework import Agent
 from agent_framework.openai import OpenAIChatClient
-from azure.identity.aio import DefaultAzureCredential, get_bearer_token_provider
 from dotenv import load_dotenv
-from rich import print
+
+load_dotenv()
